ac36f504814c61562c2985f0aa4b3bbf5f89816f
suyashworks <111303328+suyashworks@users.noreply.github.com>
2022-08-15 22:42:01 +0530

    Add files via upload

---
+++ src/main/java/com/example/springbootcrud/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbootcrud.repository;
+
+import com.example.springbootcrud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
